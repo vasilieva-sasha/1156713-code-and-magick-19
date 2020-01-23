1c631e38d67fc002f8ee770ef10e547767237dbb
Sasha <vasilieva.aleksandra@bk.ru>
2020-01-23 22:42:22 +0300

    гистограмма

--- js/stat.js
+++ js/stat.js
@@ -7,9 +7,10 @@ var COORD_Y = 10;
 var GAP = 10;
 var CLOUD_COLOR = '#fff';
 var SHADOW_COLOR = 'rgba(0, 0, 0, 0.7)';
-var BAR_GAP = 50;
+var TEXT_COLOR = '#000';
+var BAR_GAP = 30;
 var BAR_WIDTH = 40;
-
+var maxBar = 150;
 
 function drawCloud(ctx, x, y, color) {
   ctx.fillStyle = color;
@@ -17,41 +18,35 @@ function drawCloud(ctx, x, y, color) {
 }
 
 window.renderStatistics = function (ctx, names, times) {
+  var maxTime = times[0];
+
   drawCloud(ctx, COORD_X + GAP, COORD_Y + GAP, SHADOW_COLOR);
   drawCloud(ctx, COORD_X, COORD_Y, CLOUD_COLOR);
-
-  ctx.fillStyle = '#000';
+  ctx.fillStyle = TEXT_COLOR;
   ctx.font = '16px PT Mono';
   ctx.fillText('Ура вы победили!', COORD_X + BAR_WIDTH, COORD_Y + BAR_WIDTH);
-  ctx.fillText('Список результатов:', COORD_X + BAR_WIDTH, COORD_Y + (BAR_WIDTH * 2));
-
-  var maxTime = times[1];
-  var barsLength = [];
+  ctx.fillText('Список результатов:', COORD_X + BAR_WIDTH, COORD_Y + (BAR_GAP * 2));
 
   for (var i = 0; i <= times.length - 1; i++) {
+    var barsColor = 'hsla' + '(240,' + Math.floor(Math.random() * (100)) + '%,50%)';
+
+    if (names[i] === 'Вы') {
+      ctx.fillStyle = 'rgba(255, 0, 0, 1)';
+    } else {
+      ctx.fillStyle = barsColor;
+    }
+
     if (times[i] > maxTime) {
       maxTime = times[i];
     }
 
-    for (var j = 0; j <= names.length - 1; j++) {
-      barsLength[j] = (times[j] / maxTime) * (CLOUD_HEIGHT * 0.55);
+    var barsLength = times[i] / maxTime * maxBar;
 
-      if (names[j] === 'Вы') {
-        ctx.fillStyle = 'rgba(255, 0, 0, 1)';
-      } else {
-        ctx.fillStyle = barColor;
-      }
-      var barColor = 'rgb' + '(0,0,' + Math.random() * 100 * j + ')';
-      ctx.fillRect(COORD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * j, 100, BAR_WIDTH, barsLength[j]);
-    }
+    ctx.fillRect(COORD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, maxBar - barsLength + 100, BAR_WIDTH, barsLength);
+    ctx.fillStyle = TEXT_COLOR;
+    ctx.fillText(names[i], COORD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, maxBar + 120);
+    ctx.fillText(Math.round(times[i]), COORD_X + BAR_WIDTH + (BAR_WIDTH + BAR_GAP) * i, maxBar - barsLength + 90);
   }
+
+  return undefined;
 };
-
-// names[], times[]
-// barLength = times[i]
-// найти перебором самое длинное время и задать высоту полоски CLOUD_HEIGHT * 0.6
-// время разделить на худшее время и умножить на высоту полоски худшего времени
-// console.log(names);
-// console.log(times);
-// console.log(barsLength);
-
