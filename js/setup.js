73580d1cefaecbdbe731891d8a4c1121fbe967cb
Sasha <vasilieva.aleksandra@bk.ru>
2020-01-28 20:16:56 +0300

    задание нас орда

--- js/setup.js
+++ js/setup.js
@@ -7,9 +7,6 @@
   var coatColors = ['rgb(101, 137, 164)', 'rgb(241, 43, 107)', 'rgb(146, 100, 161)', 'rgb(56, 159, 117)', 'rgb(215, 210, 55)', 'rgb(0, 0, 0)'];
   var eyesColors = ['black', 'red', 'blue', 'yellow', 'green'];
 
-  var setup = document.querySelector('.setup');
-  setup.classList.remove('hidden');
-
   var characters = [
     {
       name: names[Math.floor(Math.random() * names.length)] + ' ' + surnames[Math.floor(Math.random() * surnames.length)],
@@ -33,13 +30,25 @@
     }
   ];
 
+  document.querySelector('.setup').classList.remove('hidden');
+
+  var similarListElement = document.querySelector('.setup-similar-list');
+  var similarWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
+
+  var renderWizard = function (character) {
+    var wizard = similarWizardTemplate.cloneNode(true);
+    wizard.querySelector('.setup-similar-label').textContent = character.name;
+    wizard.querySelector('.wizard-coat').style.fill = character.coatColor;
+    wizard.querySelector('.wizard-eyes').style.fill = character.eyesColor;
+    return wizard;
+  };
+
+  var fragment = document.createDocumentFragment();
+  for (var i = 0; i < characters.length; i++) {
+    fragment.appendChild(renderWizard(characters[i]));
+  }
+
+  similarListElement.appendChild(fragment);
   document.querySelector('.setup-similar').classList.remove('hidden');
-
-  var characters = document.querySelector('#similar-wizard-template');
-
-  characters
-
-
-  console.log(characters);
 
 })();
